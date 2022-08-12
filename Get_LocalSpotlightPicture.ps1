d414225c32d956a7d1c3c832cbdc5facded33b07
Raymond Lee <11689554+rleekc@users.noreply.github.com>
2022-08-12 08:18:03 -0700

    Update Get_LocalSpotlightPicture.ps1

--- Get_LocalSpotlightPicture.ps1
+++ Get_LocalSpotlightPicture.ps1
@@ -1,11 +1,12 @@
-﻿cd ~\AppData\Local\Packages\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\LocalState\Assets
+#Gets 1920x1080 spotlight background pictures and copies them to a folder in the desktop 
+
+cd ~\AppData\Local\Packages\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\LocalState\Assets
 $picturelist = New-Object System.Collections.Generic.List[string]
 foreach($picture in (get-childitem)){ if($picture.length -gt 1000000) { $picturelist.add($picture.name) } }
 foreach($s in $picturelist){$s}
 $foldername = "Spotlight_Pictures_" + (Get-Date).tostring('MM-dd-yyyy')
-New-item -itemtype Directory -Path ~\OneDrive\Desktop -Name $foldername
-$destinationDir = "~\OneDrive\Desktop\" + $foldername
+mkdir ~\OneDrive\Desktop\Spotlight -ErrorAction SilentlyContinue
+New-item -itemtype Directory -Path ~\OneDrive\Desktop\Spotlight -Name $foldername
+$destinationDir = "~\OneDrive\Desktop\Spotlight\" + $foldername
 foreach($item in $picturelist){$newName = $item + ".jpg"; Copy-Item .\$item -destination $destinationDir\$newName }
-$image = new-object -ComObject Wia.ImageFile
-cd $destinationDir
-foreach($item in (get-childitem)) {$fullPath = $destinationDir + $item; $image.loadfile("$fullPath"); if($image.Height -ne 1080 -and $image.Width -ne 1920) {Remove-Item $item}}
+pause
